a2999184ff2eab196b30663b5a4cf39bdaf8713f
tikhiy <153120124+tikhiyzima@users.noreply.github.com>
2025-09-30 11:40:40 +0200

    Update globalscript.js

--- globalscript.js
+++ globalscript.js
@@ -10,9 +10,11 @@ fetch ('contact.json').then ((res) => res.json ()).then ((res) => {
         let contact_ville = document.createElement ('td') ; contact_ville.textContent = el.city ;
         let contact_email = document.createElement ('td') ; contact_email.textContent = el.email ;
         let contact_company = document.createElement ('td') ; contact_company.textContent = el.company ;
-        let contact_skills = document.createElement ('td') ; contact_skills.textContent = el.skills ;
-        tr.append (contact_name , contact_ville , contact_email , contact_company , contact_skills) ;
+        let contact_skills = document.createElement ('td') ; 
+        contact_skills.append (...el.skills.map ((e) => { let skill_el = document.createElement ('label') ; skill_el.textContent = e ; return skill_el ; }))
 
+        tr.append (contact_name , contact_ville , contact_email , contact_company , contact_skills) ;
+        
         __pushctn.append (tr) ;
     }) ;
 
