5e222f1d13dbd617eadcd639488327f39d8b8e70
tikhiy <153120124+tikhiyzima@users.noreply.github.com>
2025-09-30 11:07:29 +0200

    Update globalscript.js

--- globalscript.js
+++ globalscript.js
@@ -2,7 +2,15 @@
 
 let __pushctn = document.getElementById ('__pushctn') ;
 fetch ('posts.json').then ((res) => res.json ()).then ((res) => {
-    console.log (res) ;
+    res.contacts.forEach((el) => {
+        console.log (el) ;
+
+        let tr = document.createElement ('tr') ;
+        let contact_name = document.createElement ('td') ; contact_name.textContent = el.Name ;
+        tr.append (contact_name) ;
+        
+        __pushctn.append (tr) ;
+    }) ;
 
 }) ;
 
